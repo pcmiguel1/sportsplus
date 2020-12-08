af6cf1137185a1fc7d53ee28d3e070f0158224d3
Brandão <zemariabrandao@gmail.com>
2020-12-08 19:12:07 +0000

    Mapa update + Start User Menu

---
+++ code/public/javascripts/mapa.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+
+    setupMap(); 
+
+}
+
+function setupMap() {
+    var map = L.map('mapa',{minZoom: 12}).setView(new L.LatLng(38.7476289, -9.1518309), 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/pcmiguel/ckhsyjp813gxb19qq3eqydsmu/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGNtaWd1ZWwiLCJhIjoiY2toc3lncG1zMGllajJxcGkxYnNjanVieCJ9.yfUra6VpwwsP4dGk9badRA', {
+        tileSize: 512,
+        zoomOffset: -1,
+        attribution: '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+}
