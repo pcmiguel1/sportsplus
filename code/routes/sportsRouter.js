e5d8aff2c1298e7ec9dccf75242279ed0466e6a2
Miguel <miguelpiresamaro@gmail.com>
2020-12-11 09:54:47 +0000

    fix error

--- code/routes/sportsRouter.js
+++ code/routes/sportsRouter.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var sportsModel = require('../models/sportsModel');
+var sportsModel = require('../Models/sportsModel');
 
 router.get('/', async function(req, res, next) {
     let result = await sportsModel.getAllSports();
